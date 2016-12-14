fe2edd15742db357fbf3552a971c2c4b848e9f8e
Laninisaleksey <Laninisaleksey@gmail.com>
2016-12-14 08:55:35 +0400

    Delete ovpn_pars.sh

--- ipchairs/ovpn_pars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ip=`cat vpngate*.ovpn | grep -E "^remote" | awk '{print $2}'`
-echo "IP Parsed: "$ip >> variables.txt
-port=`cat vpngate*.ovpn | grep -E "^remote" | awk '{print $3}'`
-echo "Port Parsed: "$port >> variables.txt
-interface=`cat vpngate*.ovpn | grep -E "^dev" | awk '{print $2}'`
-echo "Interface Parsed: "$interface >> variables.txt
